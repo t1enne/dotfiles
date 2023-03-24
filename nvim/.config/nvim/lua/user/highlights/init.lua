a1bdbd5f0d94229fde60c068761ec379eb5d8dcd
t1enne <nasir.taov@gmail.com>
2023-03-24 07:56:01 +0100

    NVIM - added transparency

--- nvim/.config/nvim/lua/user/highlights/init.lua
+++ nvim/.config/nvim/lua/user/highlights/init.lua
@@ -1,3 +1,13 @@
-return { -- this table overrides highlights in all themes
-  -- Normal = { bg = "#000000" },
+return {
+  -- this table overrides highlights in all themes
+  Normal = { bg = "NONE", ctermbg = "NONE" },
+  NormalNC = { bg = "NONE", ctermbg = "NONE" },
+  -- CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  -- CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  -- CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  -- LineNr = {},
+  -- SignColumn = {},
+  -- StatusLine = {},
+  NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+  NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
 }
