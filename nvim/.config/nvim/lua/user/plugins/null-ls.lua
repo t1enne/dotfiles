e0940613c9165465f2fb88c63b142bbafb8b58f3
t1enne <nasir.taov@gmail.com>
2023-04-12 07:52:28 +0200

    NVIM - ts lsp and prettier

--- nvim/.config/nvim/lua/user/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/user/plugins/null-ls.lua
@@ -4,11 +4,11 @@ return {
     local null_ls = require "null-ls"
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-    null_ls.register(null_ls.builtins.formatting.prettier_d_slim.with {
+    null_ls.register(null_ls.builtins.formatting.prettierd.with {
       condition = function(utils)
         return utils.root_has_file ".prettierrc"
-          or utils.root_has_file ".prettierrc.json"
-          or utils.root_has_file ".prettierrc.js"
+            or utils.root_has_file ".prettierrc.json"
+            or utils.root_has_file ".prettierrc.js"
       end,
     })
     return config -- return final config table
--- nvim/.config/nvim/lua/user/plugins/user.lua
+++ nvim/.config/nvim/lua/user/plugins/user.lua
@@ -16,4 +16,28 @@ return {
       { "<leader><F5>", "<cmd>UndotreeToggle<cr>", desc = "Toggle UndoTree" },
     },
   },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "jose-elias-alvarez/typescript.nvim",
+      -- init = function() end,
+    },
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- tsserver will be automatically installed with mason and loaded with lspconfig
+        tsserver = {},
+      },
+      -- you can do any additional lsp server setup here
+      -- return true if you don't want this server to be setup with lspconfig
+      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      setup = {
+        tsserver = function(_, opts)
+          require("typescript").setup { server = opts }
+          return true
+        end,
+      },
+    },
+  },
 }
