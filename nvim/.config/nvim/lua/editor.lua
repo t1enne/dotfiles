5fa951833f20515d0d661e79b0914bba3b026559
nasir.taov <nasir.taov@raintonic.com>
2026-03-08 09:18:15 +0100

    nvim - add ruff format

--- nvim/.config/nvim/lua/editor.lua
+++ nvim/.config/nvim/lua/editor.lua
@@ -142,10 +142,6 @@ return {
     event = { 'BufReadPre', 'BufNewFile', 'BufEnter' },
     config = function()
       local conform = require 'conform'
-
-      -- Global variable to store the current formatter
-      vim.g.current_formatter = 'None'
-
       local formatters_by_ft = {
         javascript = { 'prettierd', 'biome' },
         typescript = { 'prettierd', 'biome' },
@@ -160,7 +156,7 @@ return {
         graphql = { 'prettierd' },
         liquid = { 'prettierd' },
         lua = { 'stylua' },
-        python = { 'ruff' },
+        python = { 'ruff_format' },
       }
 
       conform.setup {
@@ -171,87 +167,13 @@ return {
         },
       }
 
-      -- Function to find the first config file by walking up the directory tree
-      local function find_first_config()
-        local current_dir = vim.fn.expand '%:p:h'
-        local home_dir = vim.fn.expand '$HOME'
-
-        local config_files = {
-          -- prettier = { '.prettierrc', '.prettierrc.json', '.prettierrc.js' },
-          biome = { 'biome.json' },
-          deno = { 'deno.json', 'deno.jsonc' },
-        }
-
-        while current_dir ~= home_dir and current_dir ~= '/' do
-          for formatter, patterns in pairs(config_files) do
-            for _, pattern in ipairs(patterns) do
-              local config_file = current_dir .. '/' .. pattern
-              if vim.fn.filereadable(config_file) == 1 then
-                return formatter
-              end
-            end
-          end
-          current_dir = vim.fn.fnamemodify(current_dir, ':h')
-        end
-        return nil
-      end
-
-      -- Function to determine the formatter based on config files and file type
-      local function get_formatter()
-        local filetype = vim.bo.filetype
-        local available_formatters = formatters_by_ft[filetype] or {}
-        local formatter = find_first_config()
-
-        if formatter then
-          if formatter == 'prettierd' and vim.tbl_contains(available_formatters, 'prettierd') then
-            vim.g.current_formatter = 'prettierd'
-            return { 'prettierd' }
-          elseif formatter == 'biome' and vim.tbl_contains(available_formatters, 'biome') then
-            vim.g.current_formatter = 'biome'
-            return { 'biome' }
-          elseif formatter == 'deno' and vim.tbl_contains(available_formatters, 'deno_fmt') then
-            vim.g.current_formatter = 'deno_fmt'
-            return { 'deno_fmt' }
-          end
-        end
-
-        -- Default to the first available formatter for the file type, or prettier if none specified
-        vim.g.current_formatter = available_formatters[1] or 'prettierd'
-        return { vim.g.current_formatter }
-      end
-
-      -- Function to update the current formatter
-      local function update_current_formatter()
-        get_formatter()
-      end
-
-      -- Create autocmd for updating the formatter on buffer open
-      vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile', 'BufEnter' }, {
-        callback = update_current_formatter,
-      })
-
       -- Create autocmd for formatting on save
       vim.api.nvim_create_autocmd('BufWritePre', {
         pattern = '*',
         callback = function(args)
-          local formatters = get_formatter()
-          conform.format {
-            bufnr = args.buf,
-            formatters = formatters,
-            timeout_ms = 1000,
-          }
+          require('conform').format { bufnr = args.buf }
         end,
       })
-
-      -- Keymap for manual formatting
-      vim.keymap.set({ 'n', 'v' }, '<leader>mp', function()
-        local formatters = get_formatter()
-        conform.format {
-          formatters = formatters,
-          async = false,
-          timeout_ms = 1000,
-        }
-      end, { desc = 'Format file or range (in visual mode)' })
     end,
   },
 }
